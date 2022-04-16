5d23062d78f81609e0dc9ca1c057484836dae479
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:35:08 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/appcluster/csync2/Csync2ResourcePropertiesConfiguration.java
+++ src/main/java/com/aoapps/appcluster/csync2/Csync2ResourcePropertiesConfiguration.java
@@ -1,6 +1,6 @@
 /*
  * ao-appcluster-csync2 - Application-level clustering tools for Csync2.
- * Copyright (C) 2011, 2015, 2016, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2011, 2015, 2016, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-appcluster-csync2.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.appcluster.csync2;
 
 import com.aoapps.appcluster.AppCluster;
